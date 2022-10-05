de0cb34ef134b10fd5859eaddecfd48ae609b408
unknown <jack.doyle.environment@gmail.com>
2022-10-05 18:50:27 +0200

    Init commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
+const e = require('express')
+const { response } = require('express')
+const ObjectID = require('mongoose').Types.ObjectId
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+const bcrypt = require('bcrypt')
+
+const myFunction = async () => {
+    const password = 'Red12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('red12345!', hashedPassword)
+    console.log(isMatch)
+
+}
+
+myFunction()
